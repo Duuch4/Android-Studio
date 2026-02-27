dd5cf857b7fef90ccc2f20343005c2775c647342
duuch4 <duchpons@gmail.com>
2026-02-27 18:34:09 +0100

    Text idioma actual

--- MiniActv22/app/src/main/java/com/example/miniactv2/MainActivity.kt
+++ MiniActv22/app/src/main/java/com/example/miniactv2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -30,10 +31,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp() {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Greeting(
-            name = stringResource(R.string.android_name),
-            modifier = Modifier.padding(innerPadding)
-        )
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+        ) {
+            Text(
+                text = stringResource(R.string.current_language)
+            )
+
+            Greeting(
+                name = stringResource(R.string.android_name),
+                modifier = Modifier
+                    .padding(innerPadding)
+            )
+        }
     }
 }
 
