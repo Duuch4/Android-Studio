f1e0f398036b36ead5cab61bbf9a208e6a781bb8
duuch4 <duchpons@gmail.com>
2026-04-08 22:43:03 +0200

    Limpieza + Botones Principal

--- Buscaminas/app/src/main/java/com/example/buscaminas/MainActivity.kt
+++ Buscaminas/app/src/main/java/com/example/buscaminas/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.buscaminas.ui.theme.BuscaminasTheme
 
@@ -29,25 +35,38 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp() {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Greeting(
-            name = "Android",
+        Principal(
             modifier = Modifier.padding(innerPadding)
         )
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Principal(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Button(onClick = { }) {
+            Text(text = stringResource(id = R.string.boton_ayuda))
+        }
+
+        ElevatedButton(onClick = { }) {
+            Text(text = stringResource(id = R.string.boton_empezar))
+        }
+
+        Button(onClick = { }) {
+            Text(text = stringResource(id = R.string.boton_salir))
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PrincipalPreview() {
     BuscaminasTheme {
-        Greeting("Android")
+        Principal()
     }
 }
