62636d525e0d43688d5db3991c7c60267c7dc7c7
duuch4 <duchpons@gmail.com>
2026-04-13 12:35:50 +0200

    First Ayuda + FixHeader iconos

--- Buscaminas/app/src/main/java/com/example/buscaminas/MainActivity.kt
+++ Buscaminas/app/src/main/java/com/example/buscaminas/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.buscaminas
 
+import android.R.id.bold
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
@@ -15,8 +21,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.buscaminas.ui.theme.BuscaminasTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +43,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun MyApp() {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -42,23 +53,96 @@ fun MyApp() {
 }
 @Composable
 fun Principal(modifier: Modifier = Modifier) {
+
     Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = modifier.fillMaxSize()
     ) {
 
-        Button(onClick = { }) {
-            Text(text = stringResource(id = R.string.boton_ayuda))
-        }
+        Header(
+            titulo = stringResource(id = R.string.menu_principal),
+            icono = R.drawable.icono_mina
+        )
 
-        ElevatedButton(onClick = { }) {
-            Text(text = stringResource(id = R.string.boton_empezar))
-        }
+        Column(
+            modifier = modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
 
-        Button(onClick = { }) {
-            Text(text = stringResource(id = R.string.boton_salir))
+            Button(onClick = { }) {
+                Text(text = stringResource(id = R.string.boton_ayuda))
+            }
+
+            ElevatedButton(onClick = { }) {
+                Text(text = stringResource(id = R.string.boton_empezar))
+            }
+
+            Button(onClick = { }) {
+                Text(text = stringResource(id = R.string.boton_salir))
+            }
         }
+    }
+}
+
+@Composable
+fun Ayuda(modifier: Modifier = Modifier, onVolver: () -> Unit) {
+
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+
+        Header(
+            titulo = stringResource(id = R.string.header_ayuda),
+            icono = R.drawable.icono_mina
+        )
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Text("ASDASd")
+
+            Button(onClick = onVolver) {
+                Text("Volver")
+            }
+        }
+    }
+}
+
+@Composable
+fun Header(titulo: String, icono: Int) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        colorResource(id = R.color.verde_header1),
+                        colorResource(id = R.color.verde_header2),
+                        colorResource(id = R.color.verde_header3),
+                    )
+                )
+            )
+            .padding(15.dp),
+        verticalAlignment = Alignment.CenterVertically //El Texto
+    ) {
+
+        Image(
+            painter = painterResource(id = icono),
+            contentDescription = stringResource(id = R.string.icono_header),
+            modifier = Modifier.size(35.dp)
+        )
+
+        Text(
+            text = titulo,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 10.dp)
+
+
+        )
     }
 }
 
@@ -68,5 +152,13 @@ fun Principal(modifier: Modifier = Modifier) {
 fun PrincipalPreview() {
     BuscaminasTheme {
         Principal()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AyudaPreview() {
+    BuscaminasTheme {
+        Ayuda(onVolver = {})
     }
 }
